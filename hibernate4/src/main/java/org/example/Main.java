989a3360df6211d544e8190db82eb4b8e0676bce
pravin jadhav <jadhavpravin7112000@gmail.com>
2024-03-02 20:00:59 +0530

    level-1 and level-2 cache

---
+++ hibernate4/src/main/java/org/example/Main.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import org.example.model.Student;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/*
+    In below program
+    a. two sessions ( level-1 cache )
+        - using first session we are fetching one object two times but due to level-1 cache query execute only one time
+        - for second session same object but cache is level-1 so it will execute query again
+        it means for every session there is seperate cache.
+    b. level-2 cache
+        - if we configure setup for level-2 (ehcache) cache
+        - for that import some anotation javax version instead of jakarta
+        - for every session there is only one cache and both cache execute query only one time.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = null;
+        Session session = null;
+        Session session1 = null;
+        Transaction transaction = null;
+        Boolean isSuccess=false;
+
+        try {
+            sessionFactory = new Configuration().configure().
+                addAnnotatedClass(Student.class).buildSessionFactory();
+
+            session = sessionFactory.openSession();
+
+            Student student = session.get(Student.class,1);
+            System.out.println(student);
+
+            Student student1 = session.get(Student.class,1);
+            System.out.println(student1);
+
+            session1 = sessionFactory.openSession();
+
+            Student student2 = session1.get(Student.class,1);
+            System.out.println(student2);
+
+            isSuccess = true;
+        }
+        catch (HibernateException e)
+        {
+            if (transaction != null)
+                transaction.rollback();
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {   if (transaction != null)
+                transaction.rollback();
+
+            e.printStackTrace();
+        }
+        finally {
+
+            if(transaction != null &&  isSuccess )
+                transaction.commit();
+
+            if( session != null && session.isOpen() )
+                session.close();
+
+            if( sessionFactory != null )
+                sessionFactory.close();
+        }
+
+    }
+}
---
+++ hibernate4/src/main/java/org/example/model/Student.java
@@ -0,0 +1,78 @@
+package org.example.model;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "students")
+
+//below two annotations for level-2 cache
+//@Cacheable
+//@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class Student {
+    @Id
+    private Integer id;
+    private String name;
+    private String email;
+    private int age;
+    private String city;
+
+    public Student() {
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
